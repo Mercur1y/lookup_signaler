9e831bbb682aa8cbb91478200e578b3f41ef8bc7
Глеб Приставка <g.pristavka@bftcom.com>
2023-12-12 23:11:27 +0400

    Initial commit

---
+++ src/main/java/com/calorie_calc/lookup_signaler/LookupSignalerApplication.java
@@ -0,0 +1,13 @@
+package com.calorie_calc.lookup_signaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookupSignalerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookupSignalerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/calorie_calc/lookup_signaler/LookupSignalerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calorie_calc.lookup_signaler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookupSignalerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
